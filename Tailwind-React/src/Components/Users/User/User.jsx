e2c3347c122acd982650cab36e6a7dc3fbb7c006
ShamimHossain1 <shamimweb78@gmail.com>
2025-02-19 00:12:40 -0500

    usenavigate

--- Tailwind-React/src/Components/Users/User/User.jsx
+++ Tailwind-React/src/Components/Users/User/User.jsx
@@ -1,13 +1,20 @@
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
 
 
 const User = ({user}) => {
+
+    const navigate = useNavigate();
+    const handleShow = () => {
+        navigate(`/user/${user.id}`);
+    }
+    
     return (
         <div key={user.id} className="bg-gray-700 p-5 m-5 rounded-lg text-center">
             <h1 className="text-2xl">Name: {user.name}</h1>
             <h1 className="text-2xl">Email: {user.email}</h1>
             <h1 className="text-2xl">Phone: {user.phone}</h1>
-            <Link to={`/user/${user.id}`}>Show Details</Link>
+            <Link to={`/user/${user.id}`}>Show Details</Link><br />
+            <button onClick={handleShow} className="btn">Show</button>
         </div>
     );
 };
--- Tailwind-React/src/Components/Users/User/UserDetails/UserDetails.jsx
+++ Tailwind-React/src/Components/Users/User/UserDetails/UserDetails.jsx
@@ -1,18 +1,23 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 const UserDetails = () => {
     const details = useLoaderData();
-   
+    const navigate = useNavigate();
+
+    const handleGoBack = () => {
+        navigate(-1);
+    }
+
     return (
         <div>
             <div className="bg-gray-700 p-5 m-5 rounded-lg text-center">
                 <h1 className="text-2xl">Name: {details.name}</h1>
                 <h1 className="text-2xl">Email: {details.email}</h1>
                 <h1 className="text-2xl">Phone: {details.phone}</h1>
-                </div>
-           
-           
+                <button onClick={handleGoBack} className="btn">Go Back</button>
+            </div>
+
         </div>
     );
 };
