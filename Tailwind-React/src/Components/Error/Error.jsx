129e534a44c741cd29fff61fc626b1dc34b89da5
ShamimHossain1 <shamimweb78@gmail.com>
2025-02-20 23:56:55 -0500

    fin error

--- Tailwind-React/src/Components/Error/Error.jsx
+++ Tailwind-React/src/Components/Error/Error.jsx
@@ -1,5 +1,5 @@
 import { useNavigate, useRouteError } from "react-router-dom";
-import { Button } from "@/components/ui/button";
+// import { Button } from "@/components/ui/button";
 
 const Error = () => {
     const error = useRouteError();  
@@ -13,9 +13,9 @@ const Error = () => {
         <div className="flex flex-col items-center justify-center h-screen bg-gray-100 p-5">
             <h1 className="text-9xl font-bold text-gray-800 mb-4 animate-bounce">Oops!!</h1>
             <h2 className="text-3xl text-gray-600 mb-8">{error?.statusText || 'Something went wrong!'}</h2>
-            <Button onClick={handleGoBack} className="px-6 py-3 text-lg bg-blue-500 text-white rounded-2xl shadow-lg hover:bg-blue-600 transition">
+            <button onClick={handleGoBack} className="px-6 py-3 text-lg bg-blue-500 text-white rounded-2xl shadow-lg hover:bg-blue-600 transition">
                 Go To Home
-            </Button>
+            </button>
         </div>
     );
 };
