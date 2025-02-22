994792c7e359f96568d67606272648a0046bd269
ShamimHossain1 <shamimweb78@gmail.com>
2025-02-21 23:39:30 -0500

    form

--- Tailwind-React/src/Components/Home/Home.jsx
+++ Tailwind-React/src/Components/Home/Home.jsx
@@ -2,6 +2,7 @@
 import NavBar from '../Header/NavBar/NavBar';
 import Footer from '../Footer/Footer';
 import { Outlet, useLocation, useNavigation } from 'react-router-dom';
+import Form from '../Form/Form';
 
 const Home = () => {
 
@@ -20,6 +21,9 @@ const Home = () => {
             {
                 navigation.state === 'loading' ? <h1 className="text-3xl text-center mt-10">Loading...</h1> : <Outlet></Outlet>
             }
+
+            <Form></Form>
+
             <Footer></Footer>
 
         </div>
