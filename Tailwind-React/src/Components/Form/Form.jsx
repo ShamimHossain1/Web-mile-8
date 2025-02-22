eee9af038527604e61d0c078b8e1bb6157e43f0b
ShamimHossain1 <shamimweb78@gmail.com>
2025-02-21 23:46:21 -0500

    from input

--- Tailwind-React/src/Components/Form/Form.jsx
+++ Tailwind-React/src/Components/Form/Form.jsx
@@ -1,9 +1,20 @@
 
 
 const Form = () => {
+
+    const handleSubmit = (e) => {
+        console.log(e.target.name.value);
+        e.preventDefault();
+        console.log('Form Submitted');
+    }
+
     return (
         <div>
-            
+            <form onSubmit={handleSubmit} action="">
+                <input type="text" name="name" />
+                <br />
+                <input type="submit" value="submit" />
+            </form>
         </div>
     );
 };
