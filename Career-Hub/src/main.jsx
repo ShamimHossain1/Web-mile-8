7b04f0b1edaf9a73b33961af7e191848cb81dd6b
ShamimHossain1 <shamimweb78@gmail.com>
2025-02-28 23:55:17 -0500

    career hub project setup

--- Career-Hub/src/main.jsx
+++ Career-Hub/src/main.jsx
@@ -1,10 +1,30 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import {
+  createBrowserRouter,
+  RouterProvider,
+} from "react-router-dom";
 import './index.css'
-import App from './App.jsx'
+import Root from './Components/Root/Root';
+import Home from './Components/Home/Home';
+
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <Root></Root>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>,
+      },
+      
+      ],
+  },
+]);
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </StrictMode>,
 )
